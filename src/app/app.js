3d929ec1c7979d00f0349dd590717f4d2041196f
Yashaswini Siddavatam <yashaswinisiddavatam9@gmail.com>
2024-06-26 18:34:19 +0530

    first commit

---
+++ src/app/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp', []);
+
+app.controller('myController', function($scope) {
+    $scope.message = 'Hello, AngularJS!';
+    $scope.submessage = 'Welcome to Kamindu Gayanthas AngularJs Tutorial'
+});
